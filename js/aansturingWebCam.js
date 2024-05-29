47b212f991fd6da486efb9b1ea70a82acdb5bdae
PhotoboothAHS <photobooth.ash@gmail.com>
2024-05-29 16:16:19 +0200

    brooker added

--- js/aansturingWebCam.js
+++ js/aansturingWebCam.js
@@ -1,1 +1,9 @@
-$('.video-container').customWebCam()
+$photo =('.video-container').customWebCam()
+
+function startup() {
+    // video = document.getElementById('video');
+    //canvas = document.getElementById('canvas');
+    photo = document.getElementsByClassName('video-container');
+    startbutton = document.getElementsByClassName('button-picture');
+
+}
