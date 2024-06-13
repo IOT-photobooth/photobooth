faaf51bca44544c8aa5c5f47b78684d2988844a6
Stef <stefpuimege@gmail.com>
2024-06-13 15:47:10 +0200

    secure connection to broker

--- js/mqttShizzle.js
+++ js/mqttShizzle.js
@@ -1,24 +1,25 @@
-const client = mqtt.connect(`ws://mqtt.eclipseprojects.io:80/mqtt`);
+// const client = mqtt.connect(`ws://mqtt.eclipseprojects.io:80/mqtt`);
+const client = mqtt.connect(`wss://mqtt.eclipseprojects.io/mqtt`); // secure connection to the broker (port 443 is default for secure connections, it's not necessary to specify)
 
-const btnTakePic = document.querySelector('#take-picture');
+const btnTakePic = document.querySelector("#take-picture");
 
 client.on("connect", () => {
-    client.subscribe("photobooth/AHS", (err) => {
-        if (!err) {
-        client.publish("presence", "Hello mqtt");
-        console.log("we have connected");
-        //client.publish
-        }
-    });
+  client.subscribe("photobooth/AHS", (err) => {
+    if (!err) {
+      client.publish("presence", "Hello mqtt");
+      console.log("we have connected");
+      //client.publish
+    }
+  });
 });
 
 client.on("message", (topic, message) => {
-    const msg = message.toString();
-    const time = new Date().toLocaleTimeString();
-    console.log(`${time} - ${msg}`);
+  const msg = message.toString();
+  const time = new Date().toLocaleTimeString();
+  console.log(`${time} - ${msg}`);
 });
 
-btnTakePic.addEventListener('click', (e) => {
-    client.publish('photobooth/AHS', 'flash');
-    console.log('button clicked')
-})
+btnTakePic.addEventListener("click", (e) => {
+  client.publish("photobooth/AHS", "flash");
+  console.log("button clicked");
+});
