9a3b29dc7a9c89895e8cda523e0dc0b2a3ab2540
Stef <stefpuimege@gmail.com>
2024-06-13 15:30:58 +0200

    UI fix

--- js/aansturingWebCam.js
+++ js/aansturingWebCam.js
@@ -1,7 +1,7 @@
 $photo =('.video-container').customWebCam()
 
 function startup() {
-    // video = document.getElementById('video');
+    //video = document.getElementById('video');
     //canvas = document.getElementById('canvas');
     photo = document.getElementsByClassName('video-container');
     startbutton = document.getElementsByClassName('button-picture');
