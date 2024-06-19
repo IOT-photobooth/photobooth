c734d464277d58c8b762f1e86c01a02b5a106990
Stef <stefpuimege@gmail.com>
2024-06-19 10:29:25 +0200

    fix camera

--- js/camera.js
+++ js/camera.js
@@ -11,90 +11,85 @@ if (navigator.mediaDevices.getUserMedia) {
       console.log(error);
     });
 }
-(() => {
-   
-  const width = 320; // We will scale the photo width to this
-  let height = 0; // This will be computed based on the input stream
 
-  let streaming = false;
+const width = 320; // We will scale the photo width to this
+let height = 0; // This will be computed based on the input stream
 
-  let video = null;
-  let canvas = null;
-  let photo = null;
-  let startbutton = null;
+let streaming = false;
 
-  function startup() {
-    video = document.getElementById("video");
-    canvas = document.getElementById("canvas");
-    photo = document.getElementById("photo");
-    startbutton = document.getElementById("startbutton");
+//let video = null;
+let canvas = null;
+let photo = null;
+let startbutton = null;
 
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then(stream => {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(err => {
-        console.error(`An error occurred: ${err}`);
-      });
+function startup() {
+  //video = document.getElementById("video");
+  canvas = document.getElementById("canvas");
+  photo = document.getElementById("photo");
+  startbutton = document.getElementById("startbutton");
 
-    video.addEventListener(
-      "canplay",
-      ev => {
-        if (!streaming) {
-          height = video.videoHeight / (video.videoWidth / width);
-          if (isNaN(height)) {
-            height = width / (4 / 3);
-          }
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error(`An error occurred: ${err}`);
+    });
 
-          video.setAttribute("width", width);
-          video.setAttribute("height", height);
-          canvas.setAttribute("width", width);
-          canvas.setAttribute("height", height);
-          streaming = true;
+  video.addEventListener(
+    "canplay",
+    (ev) => {
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth / width);
+        if (isNaN(height)) {
+          height = width / (4 / 3);
         }
-      },
-      false
-    );
-     
 
-    startbutton.addEventListener(
-      "click",
-      ev => {
-        takepicture();
-        ev.preventDefault();
-      },
-      false
-    );
+        video.setAttribute("width", width);
+        video.setAttribute("height", height);
+        canvas.setAttribute("width", width);
+        canvas.setAttribute("height", height);
+        streaming = true;
+      }
+    },
+    false
+  );
 
-    clearphoto();
-  }
+  startbutton.addEventListener(
+    "click",
+    (ev) => {
+      takepicture();
+      ev.preventDefault();
+    },
+    false
+  );
 
-  
+  clearphoto();
+}
 
-  function clearphoto() {
-    const context = canvas.getContext("2d");
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+function clearphoto() {
+  const context = canvas.getContext("2d");
+  context.fillStyle = "#AAA";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  const data = canvas.toDataURL("image/png");
+  photo.setAttribute("src", data);
+}
+
+function takepicture() {
+  const context = canvas.getContext("2d");
+  if (width && height) {
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
 
     const data = canvas.toDataURL("image/png");
     photo.setAttribute("src", data);
+  } else {
+    clearphoto();
   }
+}
 
-  function takepicture() {
-    const context = canvas.getContext("2d");
-    if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
-
-      const data = canvas.toDataURL("image/png");
-      photo.setAttribute("src", data);
-    } else {
-      clearphoto();
-    }
-  }
-
-  window.addEventListener("load", startup, false);
-})();
+//window.addEventListener("load", startup, false);
