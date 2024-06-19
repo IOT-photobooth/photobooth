8ca544b652d2d586a459f0b44154964e2244dabf
Stef <stefpuimege@gmail.com>
2024-06-19 15:22:01 +0200

    goedkeuring debug

--- js/indexUI.js
+++ js/indexUI.js
@@ -1,7 +1,13 @@
 client.on("message", (topic, message) => {
-  console.log("message received: ", message.toString());
+  console.log("message received: ", message);
   // if message is base64 encoded image, redirect to goedkeuring.html and pass the image as a query parameter
-  if (message.toString().startsWith("data:image/png;base64")) {
-    window.location.href = `goedkeuring.html?image=${message.toString()}`;
+  if (message.startsWith("data:image/png;base64")) {
+    console.log(message);
+    console.log(message.toString());
+    // wait 5 s
+    setTimeout(() => {
+      window.location.href = `goedkeuring.html?image=${message}`;
+    }, 5000);
+    //window.location.href = `goedkeuring.html?image=${message}`;
   }
 });
