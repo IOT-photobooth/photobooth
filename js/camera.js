e230f08847246c77b4cac5e23c0d64363c078648
Stef <stefpuimege@gmail.com>
2024-06-19 11:34:20 +0200

    cleanup 2

---
+++ js/camera.js
@@ -0,0 +1,95 @@
+const client = mqtt.connect(`wss://mqtt.eclipseprojects.io/mqtt`);
+
+client.on("connect", () => {
+  client.subscribe("photobooth/AHS", (err) => {
+    if (!err) {
+      client.publish("presence", "Hello from photobooth");
+      console.log("we have connected");
+    }
+  });
+});
+
+const video = document.querySelector("#videoElement");
+const canvas = document.getElementById("canvas");
+const photo = document.getElementById("photo");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(function (error) {
+      console.log("Something went wrong!");
+      console.log(error);
+    });
+}
+
+const width = 320; // We will scale the photo width to this
+let height = 0; // This will be computed based on the input stream
+
+let streaming = false;
+
+video.addEventListener(
+  "canplay",
+  (ev) => {
+    if (!streaming) {
+      height = video.videoHeight / (video.videoWidth / width);
+      if (isNaN(height)) {
+        height = width / (4 / 3);
+      }
+
+      video.setAttribute("width", width);
+      video.setAttribute("height", height);
+      canvas.setAttribute("width", width);
+      canvas.setAttribute("height", height);
+      streaming = true;
+    }
+  },
+  false
+);
+
+function clearphoto() {
+  const context = canvas.getContext("2d");
+  context.fillStyle = "#AAA";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  const data = canvas.toDataURL("image/png");
+  photo.setAttribute("src", data);
+}
+
+function takepicture() {
+  const context = canvas.getContext("2d");
+  if (width && height) {
+    canvas.width = width;
+    canvas.height = height;
+
+    context.drawImage(video, 0, 0, width, height);
+    const data = canvas.toDataURL("image/png");
+    photo.setAttribute("src", data);
+
+    // Send the image to broker
+  } else {
+    clearphoto();
+  }
+}
+
+client.on("message", (topic, message) => {
+  console.log("message received: ", message.toString());
+  if (message.toString() === "flash") {
+    for (let i = 0; i < 5; i++) {
+      setTimeout(() => {
+        document.getElementById("timer").innerText = 5 - i;
+      }, i * 1000);
+    }
+    setTimeout(() => {
+      document.getElementById("timer").innerText = "Smile!";
+      takepicture();
+    }, 5500);
+    console.log("flash");
+    console.log("picture taken");
+  }
+});
+
+$(".video-container").customWebCam();
---
+++ js/mqtt.js
@@ -0,0 +1,43 @@
+// const client = mqtt.connect(`ws://mqtt.eclipseprojects.io:80/mqtt`);
+const client = mqtt.connect(`wss://mqtt.eclipseprojects.io/mqtt`); // secure connection to the broker (port 443 is default for secure connections, it's not necessary to specify)
+
+const btnTakePic = document.querySelector("#take-picture");
+
+client.on("connect", () => {
+  client.subscribe("photobooth/AHS", (err) => {
+    if (!err) {
+      client.publish("presence", "Hello from smartphone control");
+      client.publish("photobooth/AHS", "start");
+      console.log("we have connected");
+      //client.publish
+    }
+  });
+});
+
+client.on("reconnect", () => {
+  console.log("Reconnecting...");
+});
+
+client.on("offline", () => {
+  console.log("Went offline...");
+});
+
+window.addEventListener("load", () => {
+  client.publish("photobooth/AHS", "start");
+});
+
+client.on("message", (topic, message) => {
+  const msg = message.toString();
+  const time = new Date().toLocaleTimeString();
+  console.log(`${time} - ${msg}`);
+});
+
+btnTakePic.addEventListener("click", (e) => {
+  // disable button for 10 seconds
+  btnTakePic.disabled = true;
+  client.publish("photobooth/AHS", "flash");
+  console.log("button clicked");
+  setTimeout(() => {
+    btnTakePic.disabled = false;
+  }, 10000);
+});
