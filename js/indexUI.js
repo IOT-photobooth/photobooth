76d269e3f70da5963bacff12d6bb4f0b6daaa506
Stef <stefpuimege@gmail.com>
2024-06-19 16:01:13 +0200

    fix

--- js/indexUI.js
+++ js/indexUI.js
@@ -2,6 +2,7 @@ client.on("message", (topic, message) => {
   console.log("message received: ", message.toString());
   // if message is base64 encoded image, redirect to goedkeuring.html and pass the image as a query parameter
   if (message.toString().startsWith("data:image/png;base64")) {
-    window.location.href = `goedkeuring.html?image=${message.toString()}`;
+    console.log(message.toString());
+    //window.location.href = `goedkeuring.html?image=${message.toString()}`;
   }
 });
