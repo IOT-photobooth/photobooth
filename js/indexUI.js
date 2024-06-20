7c3eb8ee3db51cb8d5dfe460c471c4009d4e419b
AnnelynnTruyens <annelynn.truyens@student.arteveldehs.be>
2024-06-20 09:27:45 +0200

    Go back if not good

--- js/indexUI.js
+++ js/indexUI.js
@@ -9,13 +9,32 @@ client.on("message", (topic, message) => {
       <img id="image" class="container__img container__img--rect" src="" alt="" />
       <p class="question" id="question">Vind je deze foto goed?</p>
       <div class="buttons" id="approval-buttons">
-        <button class="answer-button" id="answer-yes">Ja, love it!</button>
-        <button class="answer-button" id="answer-no">Nee, opnieuw!</button>
+        <button class="answer-button" id="photo-yes">Ja, love it!</button>
+        <button class="answer-button" id="photo-no">Nee, opnieuw!</button>
       </div>
     `;
 
 		const photo = document.getElementById("image");
 		console.log(message.toString());
 		photo.setAttribute("src", message.toString());
+
+		const photoGood = document.getElementById("photo-yes");
+		const photoBad = document.getElementById("photo-no");
+
+		photoBad.addEventListener("click", () => {
+			UIpage.innerHTML = `
+    <div class="UI-page" id="UIpage">
+			<img
+				class="logo"
+				src="../images/ARTEVELDE_hs_logo RGB.png"
+				alt="logo artevelde"
+			/>
+			<p class="welcome">Druk op de knop voor een nieuwe foto!</p>
+			<button class="button-picture" id="take-picture">Neem foto</button>
+		</div>
+  `;
+
+			client.publish("photobooth/AHS", "start");
+		});
 	}
 });
