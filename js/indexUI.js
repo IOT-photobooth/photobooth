0ac4af768e9ba69d81f4d95fb635d0aa2897ad26
Stef <stefpuimege@gmail.com>
2024-06-20 12:05:00 +0200

    download button

--- js/indexUI.js
+++ js/indexUI.js
@@ -1,10 +1,10 @@
 const UIpage = document.getElementById("UIpage");
 
 client.on("message", (topic, message) => {
-	console.log("message received: ", message.toString());
-	// if message is base64 encoded image, redirect to goedkeuring.html and pass the image as a query parameter
-	if (message.toString().startsWith("data:image/png;base64")) {
-		UIpage.innerHTML = `
+  console.log("message received: ", message.toString());
+  // if message is base64 encoded image, redirect to goedkeuring.html and pass the image as a query parameter
+  if (message.toString().startsWith("data:image/png;base64")) {
+    UIpage.innerHTML = `
       <img class="logo" src="../images/ARTEVELDE_hs_logo RGB.png" alt="logo artevelde" />
       <img id="image" class="container__img container__img--rect" src="" alt="" />
       <p class="question" id="question">Vind je deze foto goed?</p>
@@ -14,39 +14,66 @@ client.on("message", (topic, message) => {
       </div>
     `;
 
-		const photo = document.getElementById("image");
-		console.log(message.toString());
-		photo.setAttribute("src", message.toString());
+    const photo = document.getElementById("image");
+    console.log(message.toString());
+    photo.setAttribute("src", message.toString());
 
-		const photoGood = document.getElementById("photo-yes");
-		const photoBad = document.getElementById("photo-no");
+    const photoGood = document.getElementById("photo-yes");
+    const photoBad = document.getElementById("photo-no");
 
-		photoBad.addEventListener("click", () => {
-			window.location.href = "../UI/index.html";
+    photoBad.addEventListener("click", () => {
+      window.location.href = "../UI/index.html";
 
-			client.publish("photobooth/AHS", "start");
-		});
+      client.publish("photobooth/AHS", "start");
+    });
 
-		photoGood.addEventListener("click", () => {
-			const question = document.getElementById("question");
-			const buttons = document.getElementById("approval-buttons");
+    photoGood.addEventListener("click", () => {
+      const question = document.getElementById("question");
+      const buttons = document.getElementById("approval-buttons");
 
-			question.innerText = "Mogen we deze tonen op de projectie?";
-			buttons.innerHTML = `
+      question.innerText = "Mogen we deze tonen op de projectie?";
+      buttons.innerHTML = `
         <button class="answer-button" id="photo-public">Ja</button>
         <button class="answer-button" id="photo-private">Nee</button>
       `;
 
-			const photoPublic = document.getElementById("photo-public");
-			const photoPrivate = document.getElementById("photo-private");
+      const photoPublic = document.getElementById("photo-public");
+      const photoPrivate = document.getElementById("photo-private");
+      const downloadDiv = document.getElementById("download");
 
-			photoPublic.addEventListener("click", () => {
-				console.log("deze foto is public");
-			});
+      photoPublic.addEventListener("click", () => {
+        console.log("deze foto is public");
+        downloadDiv.innerHTML = `<button class="download-button" id="download-button">Download</button>`;
+        const downloadButton = document.getElementById("download-button");
+        downloadButton.addEventListener("click", () => {
+          downloadImage(message.toString());
+        });
+      });
 
-			photoPrivate.addEventListener("click", () => {
-				console.log("deze foto is private");
-			});
-		});
-	}
+      photoPrivate.addEventListener("click", () => {
+        console.log("deze foto is private");
+        downloadDiv.innerHTML = `<button class="download-button" id="download-button">Download</button>`;
+        const downloadButton = document.getElementById("download-button");
+        downloadButton.addEventListener("click", () => {
+          downloadImage(message.toString());
+        });
+      });
+
+      function downloadImage(dataUrl) {
+        // Create a temporary link element
+        const link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = "photo.png"; // You can change the default name of the downloaded file
+
+        // Append link to the body
+        document.body.appendChild(link);
+
+        // Programmatically click the link to trigger the download
+        link.click();
+
+        // Remove the link from the document
+        document.body.removeChild(link);
+      }
+    });
+  }
 });
